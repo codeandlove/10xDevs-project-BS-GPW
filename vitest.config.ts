23bbe9d7a0f7837b684b408147a0fa4c757d6e1f
Piotr Makowski <codeandlove@gmail.com>
2026-01-10 23:12:29 +0100

    Add E2E setup step in GitHub Actions and update Vitest config for thresholds

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  // @ts-expect-error - Type mismatch between Vite versions in Vitest and plugins
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
@@ -15,10 +16,12 @@ export default defineConfig({
       reporter: ["text", "json", "html", "lcov"],
       exclude: ["node_modules/", "src/test/", "**/*.d.ts", "**/*.config.*", "**/mockData", "dist", ".astro"],
       all: true,
-      lines: 70,
-      functions: 70,
-      branches: 70,
-      statements: 70,
+      thresholds: {
+        lines: 70,
+        functions: 70,
+        branches: 70,
+        statements: 70,
+      },
     },
     mockReset: true,
     restoreMocks: true,
