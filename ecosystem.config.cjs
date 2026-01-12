776e7e425944e6fd049860d004c869d1f224c2d3
Piotr Makowski <codeandlove@gmail.com>
2026-01-12 23:23:55 +0100

    Update deployment configuration and add ecosystem file for PM2 management

---
+++ ecosystem.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: "BSGPW",
+      script: "./dist/server/entry.mjs",
+
+      cwd: "/home/deploy/apps/bsgpw",
+
+      instances: 1,
+      exec_mode: "fork",
+
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+        HOST: "0.0.0.0",
+      },
+    },
+  ],
+};
